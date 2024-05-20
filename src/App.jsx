8a120bae5026b335d56af22a5a1d32169ceaed60
Nika3333 <103197037+Nika3333@users.noreply.github.com>
2024-05-20 18:35:56 +0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import NavBar from "./NavBar.jsx";
 import NavRoutes from "./NavRoutes.jsx"
-import Home from "./Home.jsx";
 import Footer from "./Footer.jsx";
 
 export default function App() {
   return (
     <>
       <NavBar/>
-      <Home/>
+      <NavRoutes/>
       <Footer/>
     </>
   )
